9a72668f7578b20f5c0f28b58151ac94d0e510cf
bolliy <stephan@mante.info>
2019-02-12 20:37:31 +0100

    Version 0.0.1

--- server.js
+++ server.js
@@ -1,2 +1,65 @@
 'use strict';
-12112
+
+const express = require('express');
+const url = require('url');
+const axios = require('axios');
+
+var SERVER_IP = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+const SAPCAI_REQUEST_TOKEN = 'XXX';
+
+// Setting up our server
+express()
+  // This route will be triggered when Chatfuel sends a message
+  .get('/', (req, res) => {
+    const query = url.parse(req.url, true).query;
+    const userId = query['chatfuel user id'];
+    const userMessage = query['user_message'];
+
+    // Call SAP Conversational AI API with the user message
+    return axios
+      .post(
+        'https://api.cai.tools.sap/build/v1/dialog',
+        {
+          message: { content: userMessage, type: 'text' },
+          conversation_id: userId,
+        },
+        { headers: { Authorization: `Token ${SAPCAI_REQUEST_TOKEN}` } }
+      )
+      .then(body => {
+        // Format messages to Chatfuel format
+        const formattedMessages = body.data.results.messages.map(chatfuelFormat);
+
+        // Sends the answer back to Chatfuel
+        res.json({
+          messages: formattedMessages,
+        });
+      });
+  })
+  .listen(PORT,SERVER_IP, () => console.log(`App started on port ${PORT}`));
+
+// We need to manually "translate" SAP Conversational AI message object to Chatfuel's format
+// For now I only implemented text and images but you can add others if you need to
+// Check out the documentation I linked above to the see the json structure of the other formats
+function chatfuelFormat(message) {
+  // Source : { type: 'text', content: 'XXX' }
+  // Destination { text: 'XXX' }
+  if (message.type === 'text') {
+    return { text: message.content };
+  }
+
+  // Source: { type: 'picture', content: 'URL' }
+  // Destination: { attachment: { type: 'image', payload: { url: 'URL' } } }
+  if (message.type === 'picture') {
+    return {
+      attachment: {
+        type: 'image',
+        payload: { url: message.content },
+      },
+    };
+  }
+
+  console.error('Unsupported message format: ', message.type);
+  return { text: 'An error occured' };
+}
